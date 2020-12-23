5808f6ff8580d5466e01f3d20d94b6e7b49118b5
Derek <derek3ton@hotmail.com>
2020-12-23 13:54:09 -0800

    update
    Added assignments

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, world")
+function My_First_Function() {
+		var string = "Kiss me, I'm Irish!";
+		var result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result; 
+	}
+
